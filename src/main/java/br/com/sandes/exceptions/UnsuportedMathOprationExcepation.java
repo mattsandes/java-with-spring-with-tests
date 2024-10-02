670392595646300d0ff0bc0121898b726d07d560
Mateus Sandes <mateussandes@hotmail.com.br>
2024-10-01 22:58:01 -0300

    Criada a classe de exceções customizadas

---
+++ src/main/java/br/com/sandes/exceptions/UnsuportedMathOprationExcepation.java
@@ -0,0 +1,14 @@
+package br.com.sandes.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.BAD_REQUEST)
+public class UnsuportedMathOprationExcepation extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+	
+	public UnsuportedMathOprationExcepation(String ex) {
+		super(ex);
+	}
+}
