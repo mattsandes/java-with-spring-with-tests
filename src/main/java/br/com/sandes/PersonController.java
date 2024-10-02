7c7f15b7189d0541cbeb71f414ade5287620f2bb
Mateus Sandes <mateussandes@hotmail.com.br>
2024-10-01 23:31:08 -0300

    Inserida dependencia de PersonService

--- src/main/java/br/com/sandes/PersonController.java
+++ src/main/java/br/com/sandes/PersonController.java
@@ -1,56 +1,28 @@
 package br.com.sandes;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import br.com.sandes.exceptions.UnsuportedMathOprationExcepation;
-import br.com.sandes.service.MathService;
-import br.com.sandes.utils.ArgValidation;
+import br.com.sandes.model.Person;
+import br.com.sandes.service.PersonService;
 
 @RestController
-public class MathController {
+@RequestMapping("/person")
+public class PersonController {
 	
-	private static final MathService service = new MathService();
+	@Autowired
+	private PersonService service;
 	
-	@RequestMapping(value = "/sum/{numberOne}/{numberTwo}",
-			 method = RequestMethod.GET)
-	public Double sum(
-			@PathVariable(value = "numberOne") String numberOne,
-			@PathVariable(value = "numberTwo") String numberTwo) throws Exception {
+	@RequestMapping(
+			value = "/{id}",
+			 method = RequestMethod.GET,
+			 produces = MediaType.APPLICATION_JSON_VALUE)
+	public Person findById(@PathVariable(value = "id") String id) throws Exception {
 		
-		return service.sum(numberOne, numberTwo);
-		
-	}
-	
-	@RequestMapping(value = "/sub/{numberOne}/{numberTwo}",
-			method = RequestMethod.GET)
-	public Double sub(
-			@PathVariable(value = "numberOne") String numberOne,
-			@PathVariable(value = "numberTwo") String numberTwo) throws Exception {
-		
-		return service.sub(numberOne, numberTwo);
-		
-	}
-	
-	@RequestMapping(value = "/mult/{numberOne}/{numberTwo}",
-			method = RequestMethod.GET)
-	public Double mult(
-			@PathVariable(value = "numberOne") String numberOne,
-			@PathVariable(value = "numberTwo") String numberTwo) throws Exception {
-		
-		return service.mult(numberOne, numberTwo);
-		
-	}
-	
-	@RequestMapping(value = "/div/{numberOne}/{numberTwo}",
-			method = RequestMethod.GET)
-	public Double div(
-			@PathVariable(value = "numberOne") String numberOne,
-			@PathVariable(value = "numberTwo") String numberTwo) throws Exception {
-		
-		return service.div(numberOne, numberTwo);
-		
+		return service.findById(id);
 	}
 }
