20ecb55b52f33fc3d8d2d79d3d720287d69bede3
Mateus Sandes <mateussandes@hotmail.com.br>
2024-10-01 22:58:39 -0300

    Organizada o MathController

--- src/main/java/br/com/sandes/MathController.java
+++ src/main/java/br/com/sandes/MathController.java
@@ -5,44 +5,52 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.sandes.exceptions.UnsuportedMathOprationExcepation;
+import br.com.sandes.service.MathService;
+import br.com.sandes.utils.ArgValidation;
+
 @RestController
 public class MathController {
-
+	
+	private static final MathService service = new MathService();
+	
 	@RequestMapping(value = "/sum/{numberOne}/{numberTwo}",
 			 method = RequestMethod.GET)
 	public Double sum(
 			@PathVariable(value = "numberOne") String numberOne,
 			@PathVariable(value = "numberTwo") String numberTwo) throws Exception {
 		
-		if(!isNumeric(numberOne) || !isNumeric(numberTwo)) {
-			throw new Exception();
-		}
-		
-		return converToDouble(numberOne) + converToDouble(numberTwo);
+		return service.sum(numberOne, numberTwo);
 		
 	}
-
-	private Double converToDouble(String strNumber) {
-		if(strNumber == null) {
-			return 0D;
-		}
+	
+	@RequestMapping(value = "/sub/{numberOne}/{numberTwo}",
+			method = RequestMethod.GET)
+	public Double sub(
+			@PathVariable(value = "numberOne") String numberOne,
+			@PathVariable(value = "numberTwo") String numberTwo) throws Exception {
 		
-		String number = strNumber.replaceAll(",", ".");
+		return service.sub(numberOne, numberTwo);
 		
-		if(isNumeric(number)) {
-			return Double.parseDouble(number);
-		}
-		
-		return 0D;
 	}
-
-	private boolean isNumeric(String strNumber) {
-		if(strNumber == null) {
-			return false;
-		}
+	
+	@RequestMapping(value = "/mult/{numberOne}/{numberTwo}",
+			method = RequestMethod.GET)
+	public Double mult(
+			@PathVariable(value = "numberOne") String numberOne,
+			@PathVariable(value = "numberTwo") String numberTwo) throws Exception {
 		
-		String number = strNumber.replaceAll(",", ".");
+		return service.mult(numberOne, numberTwo);
 		
-		return number.matches("[-+]?[0-9]*\\.?[0-9]+");
+	}
+	
+	@RequestMapping(value = "/div/{numberOne}/{numberTwo}",
+			method = RequestMethod.GET)
+	public Double div(
+			@PathVariable(value = "numberOne") String numberOne,
+			@PathVariable(value = "numberTwo") String numberTwo) throws Exception {
+		
+		return service.div(numberOne, numberTwo);
+		
 	}
 }
