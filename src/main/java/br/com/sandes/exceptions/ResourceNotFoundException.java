44f891db4911f31548938a9f97b9e7e476204134
Mateus Sandes <mateussandes@hotmail.com.br>
2024-10-03 23:42:47 -0300

    Classe renomeada para ResourceNotFoundException

--- src/main/java/br/com/sandes/exceptions/ResourceNotFoundException.java
+++ src/main/java/br/com/sandes/exceptions/ResourceNotFoundException.java
@@ -3,12 +3,12 @@ package br.com.sandes.exceptions;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(HttpStatus.BAD_REQUEST)
-public class UnsuportedMathOprationExcepation extends RuntimeException {
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class ResourceNotFoundException extends RuntimeException {
 
 	private static final long serialVersionUID = 1L;
 	
-	public UnsuportedMathOprationExcepation(String ex) {
+	public ResourceNotFoundException(String ex) {
 		super(ex);
 	}
 }
