2112a933b34b7b64ae7559ecd0fa7e1532b3df87
Mateus Sandes <mateussandes@hotmail.com.br>
2024-10-05 11:19:46 -0300

    Adicionado o metodo findByJPQL

--- src/main/java/br/com/sandes/repositories/PersonRepository.java
+++ src/main/java/br/com/sandes/repositories/PersonRepository.java
@@ -1,6 +1,7 @@
 package br.com.sandes.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import br.com.sandes.model.Person;
@@ -11,4 +12,8 @@ import java.util.Optional;
 public interface PersonRepository extends JpaRepository<Person, Long>{
 
     Optional<Person> findByEmail(String email);
+
+    //criando uma query jpql;
+    @Query("select p from Person p where p.firstName = ?1 and p.lastName = ?2")
+    Person findByJPQL(String firstName, String lastName);
 }
