3122bf604246cad5e6dc658262bbddde12bdeec2
Mateus Sandes <mateussandes@hotmail.com.br>
2024-10-05 11:26:34 -0300

    Adicionado o metodo findByJPQLNamedParameters

--- src/main/java/br/com/sandes/repositories/PersonRepository.java
+++ src/main/java/br/com/sandes/repositories/PersonRepository.java
@@ -2,6 +2,7 @@ package br.com.sandes.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import br.com.sandes.model.Person;
@@ -16,4 +17,10 @@ public interface PersonRepository extends JpaRepository<Person, Long>{
     //criando uma query jpql;
     @Query("select p from Person p where p.firstName = ?1 and p.lastName = ?2")
     Person findByJPQL(String firstName, String lastName);
+
+    //criando usando query parameters;
+    @Query("select p from Person p where p.firstName =:firstName and p.lastName =:lastName")
+    Person findByJPQLNamedParameters(
+            @Param("firstName") String firstName,
+            @Param("lastName") String lastName);
 }
