f73c0202ef59ea897a85121acaaa5f92fe426bd2
Mateus Sandes <mateussandes@hotmail.com.br>
2024-10-11 08:04:03 -0300

    Adicionado testes para validar a criação da pagina do Swagger

--- src/test/java/br/com/sandes/integrationtests/swagger/SwaggerIntegrationTest.java
+++ src/test/java/br/com/sandes/integrationtests/swagger/SwaggerIntegrationTest.java
@@ -1,13 +1,32 @@
 package br.com.sandes.integrationtests.swagger;
 
+import br.com.sandes.config.TestConfig;
+import br.com.sandes.integrationtests.testcontainers.AbstractIntegrationTest;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class StartAppTests {
+import static io.restassured.RestAssured.given;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+class SwaggerIntegrationTest extends AbstractIntegrationTest {
+
+	@DisplayName("JUnit Should Display Swagger Ui Page")
 	@Test
-	void contextLoads() {
-	}
+	void testShouldDisplaySwaggerUiPage() {
 
+		var content = given()
+			.basePath("/swagger-ui/index.html")
+			.port(TestConfig.SERVER_PORT)
+		.when()
+			.get()
+		.then()
+				.statusCode(200)
+			.extract()
+				.body()
+				.asString();
+
+		assertTrue(content.contains("Swagger UI"));
+	}
 }
